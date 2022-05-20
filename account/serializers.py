398ff60fb131dfc17892494d3566d7c4d77420d2
faisal <faisal@gmail.com>
2022-05-20 04:58:35 -0700

    show user profile after bearer token athantication and login

--- account/serializers.py
+++ account/serializers.py
@@ -25,8 +25,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+
 class UserLoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255)
     class Meta:
         model = User
         fields = ['email', 'password']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'name']
