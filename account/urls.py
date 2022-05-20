3606c81f260259866acc85afaddcc2b90a1f1eb3
faisal <faisal@gmail.com>
2022-05-20 03:06:19 -0700

    user created by postman test ok

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from account.views import UserRegistrationView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name="register"),
+
+]
--- account/views.py
+++ account/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
+from account.serializers import UserRegistrationSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'message':'Registration Success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
