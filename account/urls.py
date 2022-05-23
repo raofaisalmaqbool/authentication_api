cacbeb62fa59804d9e51538615d5609fa959c88b
faisal <faisal@gmail.com>
2022-05-23 03:19:59 -0700

    create url of reset by mail

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from account.views import UserChangePasswrodView, UserRegistrationView, UserLoginView, UserProfileView
+from account.views import SendPasswordResetEmailView, UserChangePasswrodView, UserRegistrationView, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
     path('profile/', UserProfileView.as_view(), name="profile"),
     path('changepassword/', UserChangePasswrodView.as_view(), name="changepassword"),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name="send-reset-password-email"),
 
 ]
