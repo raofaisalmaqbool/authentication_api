1d2f69f4caf6ae60f66dc932a4ec15d2d0421a11
faisal <faisal@gmail.com>
2022-05-23 04:04:49 -0700

    reset password by sending email complete

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from account.views import SendPasswordResetEmailView, UserChangePasswrodView, UserRegistrationView, UserLoginView, UserProfileView
+from account.views import SendPasswordResetEmailView, UserChangePasswrodView, UserPasswordResetView, UserRegistrationView, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name="register"),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name="profile"),
     path('changepassword/', UserChangePasswrodView.as_view(), name="changepassword"),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name="send-reset-password-email"),
+    path('rest-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='rest-password'),
 
 ]
